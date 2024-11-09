7e86065bea7c1a04e031c12694f9d679097fc607
figboy9 <figboy9@tuta.io>
2024-11-09 16:27:19 +0900

    add new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/new_expense.dart';
 import 'package:flutter_expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
@@ -27,7 +28,7 @@ class _ExpensesState extends State<Expenses> {
     )
   ];
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(context: context, builder: (ctx) => Text('data'));
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
   }
 
   @override
