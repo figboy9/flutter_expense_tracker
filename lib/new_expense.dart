0ffb6f33b6b2d110967aebc8d3358482b7a32f41
figboy9 <figboy9@tuta.io>
2024-11-09 17:08:01 +0900

    add texteditingcontroller to NewExpense

--- lib/new_expense.dart
+++ lib/new_expense.dart
@@ -10,9 +10,17 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  // var _enteredTitle = '';
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleController = TextEditingController();
+  @override
+  void dispose() {
+    _titleController.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -22,7 +30,7 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text('title'),
@@ -32,7 +40,7 @@ class _NewExpenseState extends State<NewExpense> {
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_enteredTitle);
+                  print(_titleController.text);
                 },
                 child: Text('Save Expense'),
               )
